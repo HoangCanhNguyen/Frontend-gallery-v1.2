de30fce1e31fd538a54b63f8d5ada8cf511314b1
xzneozx96 <tuananhngyen218@gmail.com>
2020-09-03 10:49:22 +0700

    fix problem with aos library

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./console.component.css']
 })
 export class ConsoleComponent implements OnInit {
-  showFiller = false;
+  isOpened = true;
   constructor() { }
 
   ngOnInit(): void {
