d3e50f120e9cd070d739997341ceb35c52f3b571
HoangCanhNguyen <canh.chn@gmail.com>
2020-09-11 09:27:08 +0700

    Admin notification page

--- src/app/admin/account-manager/account-manager.component.ts
+++ src/app/admin/account-manager/account-manager.component.ts
@@ -1,30 +1,53 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
+
 import { UserService } from 'src/app/shared/service/user.service';
+import { PusherService } from 'src/app/shared/service/pusher.service';
+import { RegisterNoti } from 'src/app/shared/model/register-noti.model';
+
 @Component({
   selector: 'app-account-manager',
   templateUrl: './account-manager.component.html',
   styleUrls: ['./account-manager.component.css'],
 })
 export class AccountManagerComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   displayedColumns = ['position', 'name', 'email', 'role'];
   dataSource = new MatTableDataSource<Account>();
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  constructor(private userService: UserService) {}
+  unconfirmedRegistration: RegisterNoti[] = [];
+  registerSubcription: Subscription;
+
+  constructor(
+    private userService: UserService,
+    private _pusherService: PusherService
+  ) {}
 
   ngOnInit(): void {
-    
+    this.registerSubcription = this._pusherService
+      .getRegisterNoti()
+      .subscribe((registration) => {
+        this.unconfirmedRegistration.push(registration);
+      });
   }
+
   ngAfterViewInit(): void {
     this.userService.onGetAllAccount().subscribe((res) => {
-      this.dataSource = res
+      this.dataSource = res;
     });
     this.dataSource.paginator = this.paginator;
   }
 
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.registerSubcription.unsubscribe()
+  }
 }
+
 export interface Account {
   username: string;
   email: number;
---
+++ src/app/shared/service/pusher.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { RegisterNoti } from '../model/register-noti.model';
+
+import Pusher from 'pusher-js';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PusherService {
+  private subject: Subject<RegisterNoti> = new Subject<RegisterNoti>();
+  private pusherClient: Pusher;
+
+  constructor() {
+    this.pusherClient = new Pusher('f9c2195a4cd9fefb3c71', { cluster: 'ap1' });
+
+    const channel = this.pusherClient.subscribe('vendor_register');
+
+    channel.bind('registration', (data) => {
+      this.subject.next(
+        new RegisterNoti(
+          data.id,
+          data.username,
+          data.email,
+          data.role,
+          data.tel,
+          data.activated,
+          data.admin_confirmation
+        )
+      );
+    });
+  }
+
+  getRegisterNoti(): Observable<RegisterNoti> {
+    return this.subject.asObservable();
+  }
+}
