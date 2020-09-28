b68701d4cdb4e40f37fed2d92b6e3cb13456d84d
HoangCanhNguyen <canh.chn@gmail.com>
2020-09-28 11:50:48 +0700

    set up env for uploading to server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyASQgm-YneI6nZBiw6Hrtqp2iP5KTptinA",
+    authDomain: "gallery-47bf9.firebaseapp.com",
+    databaseURL: "https://gallery-47bf9.firebaseio.com",
+    projectId: "gallery-47bf9",
+    storageBucket: "gallery-47bf9.appspot.com",
+    messagingSenderId: "1091366238175",
+    appId: "1:1091366238175:web:1847c508ada3edc2a4b66a",
+    measurementId: "G-P8Y8SE43CC"
+  }
 };
